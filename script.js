1c86dd8e729989aa61e2a87a6cfbcd1fd5b79887
Liliane <159821845+lilianesoares1@users.noreply.github.com>
2025-08-11 17:52:54 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const toggleSenha = document.getElementById("toggleSenha");
+const campoSenha = document.getElementById("senha");
+
+toggleSenha.addEventListener("click", () => {
+  const tipo = campoSenha.type === "password" ? "text" : "password";
+  campoSenha.type = tipo;
+
+  // Troca o ícone quando mostrar/esconder
+  toggleSenha.textContent = tipo === "password" ? "👁" : "👁";
+});
